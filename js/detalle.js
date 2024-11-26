38369346437554cbd23a65e7130e31656aa0da1c
Matias K <matiaskelman.com>
2024-11-26 15:22:57 -0300

    Avances detalle.hrml

--- js/detalle.js
+++ js/detalle.js
@@ -4,8 +4,13 @@ let id = queryStrinObj.get("id");
 
 let informacion = document.querySelector(".informacion");
 
-fetch(`https://dummyjson.com/recipes/${id}`).then(function(response){return response.json()}).then(function(data){
-    informacion.innerHTML += `<p>${data.name}</p>`
-})
+fetch(`https://dummyjson.com/recipes/${id}`).then(function (response) { return response.json() }).then(function (data) {
+    console.log(data);
+    informacion.innerHTML += `<h1 class="h1Detalle">${data.name}</h1> <img src="${data.image}" alt="imagen${data.name}" srcset=""> `
+    for (let i = 0; i < data.ingredients.length; i++) {
+        informacion.innerHTML += `<li>${data.ingredients[i]}</li>`;
+    }
+    })
 
-agreg
+  
+
