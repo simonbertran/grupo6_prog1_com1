8a4b00fbb229f97f99188d1084b665dbcae3a765
Matias K <matiaskelman.com>
2024-11-25 22:34:47 -0300

    Avances

--- js/index.js
+++ js/index.js
@@ -1,22 +1,56 @@
-fetch('https://dummyjson.com/recipes?limit=10&select=name,image,difficulty')
-.then(res => res.json())
-.then(function(data){console.log(data.recipes);
+// fetch('https://dummyjson.com/recipes?limit=10&select=name,image,difficulty')
+// .then(res => res.json())
+// .then(function(data){console.log(data.recipes);
 
-    let info=data.recipes;
-    let RecetaHome=document.querySelector(".RecetasHome")
-    let diezRecetas= "";
-    for (let i=0;i< info.length; i++){
-        diezRecetas += `<li> <a href = "Receta.html?id=${info[i].id}">
+//     let info=data.recipes;
+//     let RecetaHome=document.querySelector(".RecetasHome")
+//     let diezRecetas= "";
+//     for (let i=0;i< info.length; i++){
+//         diezRecetas += `<li> <a href = "Receta.html?id=${info[i].id}">
 
-        <img src=${info[i].image}>
-        <p> ${info[i].name}</p>
-        <p> dificultad:${info[i].difficulty}</p></a> 
-        </li>`
+//         <img src=${info[i].image}>
+//         <p> ${info[i].name}</p>
+//         <p> dificultad:${info[i].difficulty}</p></a> 
+//         </li>`
 
+//     }
+//     RecetaHome.innerHTML= diezRecetas; 
+
+
+// })
+// .catch(function(error){alert("error")})
+
+
+// let rafa = fetch('https://dummyjson.com/recipes')
+// .then(function(response){return response.json();})
+// .then(function(data){return data;})
+// console.log(rafa);
+
+let cards =document.querySelector(".card_article")
+
+
+fetch('https://dummyjson.com/recipes')
+    .then(function (response) { return response.json(); })
+    .then(function (data) {
+        let recetas = data;
+
+        console.log(recetas.recipes[1]);
+
+        agregarRecetas(recetas, 10);
+
+    })
+
+
+
+function agregarRecetas(recetas, x) {
+    for (let i = 0; i < x; i++) {
+        console.log(recetas.recipes[i]);
+        cards += `<div class="card${i}"><img src="" alt="fotoReceta"><h3>${recetas.recipes[i].name}</h3><p>dificultad: </p><h5>link</h5></div>`;
     }
-    RecetaHome.innerHTML= diezRecetas; 
+}
 
-    
-})
-.catch(function(error){alert("error")})
+agregarRecetas(5);
 
+// let campoBuscar = document.querySelector(".buscarInput");
+
+// campoBuscar.addEventListener('input', function () { })
