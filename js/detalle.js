ba5c0d351b9b3a9c0551fb29a08e08ad00e31f69
Matias K <matiaskelman.com>
2024-11-26 12:50:57 -0300

    detalle.html functional

--- js/detalle.js
+++ js/detalle.js
@@ -1,6 +1,11 @@
 let queryString = location.search;
 let queryStrinObj = new URLSearchParams(queryString);
-
 let id = queryStrinObj.get("id");
 
-fetch(`https://dummyjson.com/recipes/${id}`);
+let informacion = document.querySelector(".informacion");
+
+fetch(`https://dummyjson.com/recipes/${id}`).then(function(response){return response.json()}).then(function(data){
+    informacion.innerHTML += `<p>${data.name}</p>`
+})
+
+agreg
