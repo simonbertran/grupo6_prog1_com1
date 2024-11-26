658e71b818d0b79c41afe2fc816141d37058b932
Matias K <matiaskelman.com>
2024-11-26 15:28:28 -0300

    Arreglar link css

--- js/detalle.js
+++ js/detalle.js
@@ -7,10 +7,11 @@ let informacion = document.querySelector(".informacion");
 fetch(`https://dummyjson.com/recipes/${id}`).then(function (response) { return response.json() }).then(function (data) {
     console.log(data);
     informacion.innerHTML += `<h1 class="h1Detalle">${data.name}</h1> <img src="${data.image}" alt="imagen${data.name}" srcset=""> `
+
+    })
+    
     for (let i = 0; i < data.ingredients.length; i++) {
         informacion.innerHTML += `<li>${data.ingredients[i]}</li>`;
     }
-    })
-
   
 
