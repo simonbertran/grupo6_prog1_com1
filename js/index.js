129c43bf6081322ef2080b7975cfe7503f454928
Matias K <matiaskelman.com>
2024-11-27 13:48:15 -0300

    CSS mejorado, mejoras index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 let cards = document.querySelector(".card_article");
 // let botonAgregar = 
 buttonAgregar = document.querySelector(".botonAgregar")
+let cantidadRecetas = 0;
 
 function agregarRecetas() {
     fetch('https://dummyjson.com/recipes')
@@ -12,10 +13,15 @@ function agregarRecetas() {
 }
 
 function agregarRecetasScreen(recetas, x) {
-    for (let i = 0; i < x; i++) {
+    if (cantidadRecetas >= 30){
+        alert("Ya se muestran todas las recetas!");
+    }
+    else{
+    for (let i = cantidadRecetas; i < cantidadRecetas+x; i++) {
         console.log(recetas.recipes[i]);
         cards.innerHTML += `<div class="card card${i}"><img src="${recetas.recipes[i].image}" alt="fotoReceta" class="cardImg"><h3>${recetas.recipes[i].name}</h3><p>dificultad: ${recetas.recipes[i].difficulty} </p><a href ="detalle.html?id=${recetas.recipes[i].id}" class="buttonCard">Conocer más</a></div>`;
     }
+    cantidadRecetas += x; }
 }
 agregarRecetas();
 buttonAgregar.addEventListener("click", function () { agregarRecetas() });
