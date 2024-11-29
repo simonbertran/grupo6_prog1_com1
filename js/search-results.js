8cd97343b5811639fcd9c01d973ceed9456fa7b5
simonbertran <sbertran@udesa.edu.ar>
2024-11-29 11:01:33 -0300

    Arreglado Buscador

--- js/search-results.js
+++ js/search-results.js
@@ -1,4 +1,29 @@
 let queryString = location.search;
 let queryStrinObj = new URLSearchParams(queryString);
 let buscar = queryStrinObj.get("search");
-console.log(id)
+console.log(buscar);
+
+let seccion = document.querySelector('.seccion-buscar');
+
+function agregarRecetas() {
+    fetch(`https://dummyjson.com/recipes/search?q=${buscar}`)
+        .then(function (response) { 
+            return response.json(); 
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.recipes.length; i++) {
+                let receta = data.recipes[i];
+                seccion.innerHTML += `
+                    <div class="receta">
+                        <h3>${receta.name}</h3>
+                        <p>${receta.difficulty}</p>
+                    </div>
+                `;
+            }
+        })
+        .catch(function (error) {
+            console.error("Error al obtener las recetas:", error);
+        });
+}
+
+agregarRecetas();
