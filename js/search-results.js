80ce542ca0b77a1f8ed9d6e8c0b0cf23f75bb761
Matias K <matiaskelman.com>
2024-11-29 23:18:28 -0300

    casi terminado

--- js/search-results.js
+++ js/search-results.js
@@ -3,22 +3,18 @@ let queryStrinObj = new URLSearchParams(queryString);
 let buscar = queryStrinObj.get("search");
 console.log(buscar);
 
-let seccion = document.querySelector('.seccion-buscar');
+let seccion = document.querySelector('.seccion_buscar');
 
 function agregarRecetas() {
     fetch(`https://dummyjson.com/recipes/search?q=${buscar}`)
-        .then(function (response) { 
-            return response.json(); 
+        .then(function (response) {
+            return response.json();
         })
         .then(function (data) {
             for (let i = 0; i < data.recipes.length; i++) {
                 let receta = data.recipes[i];
-                seccion.innerHTML += `
-                    <div class="receta">
-                        <h3>${receta.name}</h3>
-                        <p>${receta.difficulty}</p>
-                    </div>
-                `;
+                seccion.innerHTML += `<div class="card card${i}"><img src="${data.recipes[i].image}" alt="fotoReceta" class="cardImg"><h5>${data.recipes[i].name}</h5><h6>Dificultad: ${data.recipes[i].difficulty} </h6><a href ="detalle.html?id=${data.recipes[i].id}" class="buttonCard">Conocer más</a></div>`
+                    ;
             }
         })
         .catch(function (error) {
